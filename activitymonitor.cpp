b216930d052a5a14de0aa954eecb40ac8420f12b
Trenton <tspice23@att.net>
2017-10-22 16:50:01 -0400

    added activity monitor implementation with input data

--- activitymonitor.cpp
+++ activitymonitor.cpp
@@ -1,3 +1,44 @@
 // Challenge 3: Activity Monitor
 
-// use a hash map with two ints
+
+#include <iostream>
+#include <map>
+#include <stdlib.h>
+
+class processMonitor {
+  std::map<int, int> processMap;
+  public:
+    void initProcessID(int pid);
+    int getProcessID();
+};
+
+void processMonitor::initProcessID(int pid) {
+  processMap[pid] += 1;
+}
+
+int processMonitor::getProcessID() {
+  int ret;
+  typedef std::map<int, int>::const_iterator processIterator;
+  for (processIterator pos = processMap.begin(); pos != processMap.end(); ++pos) {
+    if (pos->second % 2 == 0) {
+      ret = 0;
+    }
+    else if (pos->second % 2 != 0) {
+      ret = pos->first;
+    }
+  }
+
+  return ret;
+}
+
+int main() {
+  processMonitor p;
+  int line;
+  while(std::cin >> line) {
+    p.initProcessID(line);
+  }
+
+  int ret = p.getProcessID();
+  std::cout << ret << std::endl;
+
+}
